3d84da2bc99cf35b0c44c7c5b0c733f57c3a496c
Axmouth <aggronfg@yahoo.com>
2020-03-05 14:40:00 +0200

    Moved media and static to a bind so that native nginx can serve the files.

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -f Dockerfile -t axmouth/my_personal_site_django_server .
+docker build --mount type=bind,source=/var/lib/my_personal_site/static,target=/code/mybiosite/static -f Dockerfile -t axmouth/my_personal_site_django_server .
 docker build -f postgres/Dockerfile -t axmouth/my_personal_site_postgres .
 docker-compose up -d
