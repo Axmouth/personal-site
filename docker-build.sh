ab0964467734e57843050aef024cdbac0b9e4650
Axmouth <aggronfg@yahoo.com>
2020-03-05 22:39:30 +0200

    Removed unnecessary postgres container.

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 docker build -f Dockerfile -t axmouth/my_personal_site_django_server .
-docker build -f postgres/Dockerfile -t axmouth/my_personal_site_postgres .
 docker-compose up -d
