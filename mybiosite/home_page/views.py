ad544203b3a87497f10c3139589da09ee763cbee
Axmouth <aggronfg@yahoo.com>
2020-03-06 13:10:57 +0200

    Added the contact form. Moved all sidewidgets to their own templates for less clutter and easy reuse.

--- mybiosite/home_page/views.py
+++ mybiosite/home_page/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail, BadHeaderError
+
 from projects.models import Project
 from home_page.models import Link, TextBody
+from home_page.forms import ContactForm
+
+from mybiosite.settings import EMAIL_HOST
 
 
 def get_links():
@@ -30,6 +36,30 @@ def home_page(request):
     projects = Project.objects.all()
     context['projects'] = projects
     return render(request, 'home_page.html', context)
+
+
+def contact_me(request):
+    context = get_base_context()
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['contact@giorgosnikolop.info'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    context['form'] = form
+    return render(request, 'contact_me.html', context)
+
+
+def contact_success(request):
+    context = get_base_context()
+    return render(request, 'contact_success.html', context)
 
 
 def my_page(request):
--- mybiosite/mybiosite/settings.py
+++ mybiosite/mybiosite/settings.py
@@ -113,10 +113,6 @@ DEFAULT_FROM_EMAIL = "Helpdesk <helpdesk@giorgosnikolop.info>"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'sqlite': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DJANGO_POSTGRES_DB', 'my_bio_site'),
@@ -168,13 +164,11 @@ STATIC_URL = '/static/'
 
 
 if DEBUG is True:
-    #del STATIC_ROOT
     STATICFILES_DIRS = [
        os.path.join(BASE_DIR, 'static'),
     ]
 
 if DEBUG is False:
-    #del STATICFILES_DIRS
     STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -186,16 +180,16 @@ INTERNAL_IPS = [
     # ...
 ]
 
-#DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000000000000000
+# DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000000000000000
 
 SUMMERNOTE_CONFIG = {
     # Using SummernoteWidget - iframe mode, default
-    #'iframe': True,
+    # 'iframe': True,
 
     # Or, you can set it as False to use SummernoteInplaceWidget by default - no iframe mode
     # In this case, you have to load Bootstrap/jQuery stuff by manually.
     # Use this when you're already using Bootstraip/jQuery based themes.
-    #'iframe': False,
+    # 'iframe': False,
 
     # You can put custom Summernote settings
     'summernote': {
@@ -204,15 +198,15 @@ SUMMERNOTE_CONFIG = {
 
         # Change editor size
         'width': '75%%',
-        #'height': '480',
+        # 'height': '480',
 
         # Use proper language setting automatically (default)
-        #'lang': None,
+        # 'lang': None,
 
         # You can also add custom settings for external plugins
-        #'print': {
+        # 'print': {
         #    'stylesheetUrl': '/some_static_folder/printable.css',
-        #},
+        # },
         'codemirror': {
             'mode': 'htmlmixed',
             'lineNumbers': 'true',
@@ -225,27 +219,27 @@ SUMMERNOTE_CONFIG = {
     'attachment_require_authentication': True,
 
     # Set `upload_to` function for attachments.
-    #'attachment_upload_to': my_custom_upload_to_func(),
+    # 'attachment_upload_to': my_custom_upload_to_func(),
 
     # Set custom storage class for attachments.
-    #'attachment_storage_class': 'my.custom.storage.class.name',
+    # 'attachment_storage_class': 'my.custom.storage.class.name',
 
     # Set custom model for attachments (default: 'django_summernote.Attachment')
-    #'attachment_model': 'my.custom.attachment.model', # must inherit 'django_summernote.AbstractAttachment'
+    # 'attachment_model': 'my.custom.attachment.model', # must inherit 'django_summernote.AbstractAttachment'
 
     # You can disable attachment feature.
-    #'disable_attachment': False,
+    # 'disable_attachment': False,
 
     # Set `True` to return attachment paths in absolute URIs.
-    #'attachment_absolute_uri': False,
+    # 'attachment_absolute_uri': False,
 
     # test_func in summernote upload view. (Allow upload images only when user passes the test)
     # https://docs.djangoproject.com/en/2.2/topics/auth/default/#django.contrib.auth.mixins.UserPassesTestMixin
 
-    #def example_test_func(request):
+    # def example_test_func(request):
     #    return request.user.groups.filter(name='group_name').exists()
 
-    #'test_func_upload_view': example_test_func,
+    # 'test_func_upload_view': example_test_func,
 
     # You can add custom css/js for SummernoteWidget.
     'css': (
@@ -269,14 +263,14 @@ SUMMERNOTE_CONFIG = {
     # Lazy initialize
     # If you want to initialize summernote at the bottom of page, set this as True
     # and call `initSummernote()` on your page.
-    #'lazy': True,
+    # 'lazy': True,
 
     # To use external plugins,
     # Include them within `css` and `js`.
-    #'js': {
-        #'/some_static_folder/summernote-ext-print.js',
-        #'//somewhere_in_internet/summernote-plugin-name.js',
-    #},
+    # 'js': {
+    #     '/some_static_folder/summernote-ext-print.js',
+    #     '//somewhere_in_internet/summernote-plugin-name.js',
+    # },
 }
 
 # CSRF_COOKIE_SECURE = is_true_string(os.environ.get('DJANGO_CSRF_COOKIE_SECURE', 'True'))
